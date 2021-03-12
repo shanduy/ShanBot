a9f4ec032f9c7df54408ae5c44c6eaddf578bc0f
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-03-12 15:01:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `Oiin @${num.split('@')[0]}\nBem vindo ao grupo *${mdata.subject}*`
+				teks = `Mi loco @${num.split('@')[0]}\nTodo bien pa bienvenido *${mdata.subject}*`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
