1523a1f884f92e9bfdc58e2da254b43c26bd75fa
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-03-12 20:38:44 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -14,62 +14,12 @@ const help = (prefix) => {
   ├─ ❏ ${prefix}chatlist
   ├─ ❏ ${prefix}ping
   └─ ❏ ${prefix}bugreport
-◪ *HACER*
+◪ *CREAR*
   │
   ├─ ❏ ${prefix}sticker
   ├─ ❏ ${prefix}stickergif
   ├─ ❏ ${prefix}toimg
-  ├─ ❏ ${prefix}tomp3
-  ├─ ❏ ${prefix}bpink
-  ├─ ❏ ${prefix}marvellogo
-  ├─ ❏ ${prefix}snowwrite
-  ├─ ❏ ${prefix}3dtext
-  ├─ ❏ ${prefix}ninjalogo
-  ├─ ❏ ${prefix}water
-  ├─ ❏ ${prefix}firetext
-  ├─ ❏ ${prefix}logowolf
-  ├─ ❏ ${prefix}logowolf2
-  ├─ ❏ ${prefix}phlogo
-  ├─ ❏ ${prefix}glitch
-  ├─ ❏ ${prefix}neonlogo
-  ├─ ❏ ${prefix}neonlogo2
-  ├─ ❏ ${prefix}lionlogo
-  ├─ ❏ ${prefix}jokerlogo
-  ├─ ❏ ${prefix}shadow
-  ├─ ❏ ${prefix}burnpaper
-  ├─ ❏ ${prefix}coffee
-  ├─ ❏ ${prefix}lovepaper
-  ├─ ❏ ${prefix}woodblock
-  ├─ ❏ ${prefix}qowheart
-  ├─ ❏ ${prefix}mutgrass
-  ├─ ❏ ${prefix}undergocean
-  ├─ ❏ ${prefix}woodenboards
-  ├─ ❏ ${prefix}wolfmetal
-  ├─ ❏ ${prefix}metalictglow
-  ├─ ❏ ${prefix}8bit
-  ├─ ❏ ${prefix}ttp
-  ├─ ❏ ${prefix}herrypotter
-  ├─ ❏ ${prefix}pubglogo
-  └─ ❏ ${prefix}quotemaker
-◪ *MEDIA*
-  │
-  ├─ ❏ ${prefix}trendtwit
-  ├─ ❏ ${prefix}randomkpop
-  └─ ❏ ${prefix}ytsearch
-◪ *EDUCACION*
-  │
-  ├─ ❏ ${prefix}wiki
-  ├─ ❏ ${prefix}wikien
-  ├─ ❏ ${prefix}nulis
-  ├─ ❏ ${prefix}quotes
-  ├─ ❏ ${prefix}quotes2
-  └─ ❏ ${prefix}artinama
-◪ *KERANG AJAIB*
-  │
-  ├─ ❏ ${prefix}apakah
-  ├─ ❏ ${prefix}kapankah
-  ├─ ❏ ${prefix}rate
-  └─ ❏ ${prefix}bisakah
+  └─ ❏ ${prefix}tomp3 
 ◪ *DESCARGAS*
   │
   ├─ ❏ ${prefix}images
@@ -77,57 +27,10 @@ const help = (prefix) => {
   ├─ ❏ ${prefix}ytmp4
   ├─ ❏ ${prefix}tiktok
   └─ ❏ ${prefix}joox
-◪ *MEME*
-  │
-  ├─ ❏ ${prefix}meme
-  └─ ❏ ${prefix}memeindo
 ◪ *AUDIO*
   │
   ├─ ❏ ${prefix}play
   └─ ❏ ${prefix}tts
-◪ *MÚSICA*
-  │
-  ├─ ❏ ${prefix}lirik
-  └─ ❏ ${prefix}chord
-◪ *ISLAM*
-  │
-  └─ ❏ ${prefix}quran
-◪ *STALK*
-  │
-  ├─ ❏ ${prefix}tiktokstalk
-  └─ ❏ ${prefix}igstalk
-◪ *WIBU*
-  │
-  ├─ ❏ ${prefix}neonime
-  ├─ ❏ ${prefix}pokemon
-  ├─ ❏ ${prefix}loli
-  ├─ ❏ ${prefix}waifu
-  ├─ ❏ ${prefix}randomanime
-  ├─ ❏ ${prefix}husbu
-  ├─ ❏ ${prefix}husbu2
-  ├─ ❏ ${prefix}wait
-  └─ ❏ ${prefix}nekonime
-◪ *DIVERSION*
-  │
-  ├─ ❏ ${prefix}alay
-  ├─ ❏ ${prefix}gantengcek
-  ├─ ❏ ${prefix}watak
-  ├─ ❏ ${prefix}hobby
-  ├─ ❏ ${prefix}game
-  ├─ ❏ ${prefix}bucin
-  ├─ ❏ ${prefix}trust
-  ├─ ❏ ${prefix}dare
-  └─ ❏ ${prefix}simi
-◪ *INFORMACION*
-  │
-  ├─ ❏ ${prefix}bahasa
-  ├─ ❏ ${prefix}kodenegara
-  ├─ ❏ ${prefix}kbbi
-  ├─ ❏ ${prefix}fakta
-  ├─ ❏ ${prefix}infocuaca
-  ├─ ❏ ${prefix}infogempa
-  ├─ ❏ ${prefix}jadwaltvnow
-  └─ ❏ ${prefix}covid
 ◪ *DUEÑO*
   │
   ├─ ❏ ${prefix}setprefix
