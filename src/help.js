246d80c0696a26740fb96f301e9081dabcd9db3e
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-04-11 13:29:55 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -24,41 +24,69 @@ const help = (prefix) => {
   ├─ ❏ ${prefix}sticker
   ├─ ❏ ${prefix}stickergif
   ├─ ❏ ${prefix}toimg
-  └─ ❏ ${prefix}tomp3 
+  ├─ De sticker a JPG
+  ├─ ❏ ${prefix}tomp3
+  └─ De video a MP3
 
 ◪ *DESCARGAS*
   │
-  └─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}ytmp4
+  └─ Mas link del video de YT
 
 ◪ *AUDIO*
   │
   ├─ ❏ ${prefix}play
+  ├─ Mas nombre y artista
   ├─ ❏ ${prefix}bahasa
-  └─ ❏ ${prefix}tts
+  └─ ❏ ${prefix}tts es
 
-◪ *DUEÑO*
+◪ *DUEÑO DEL BOT*
   │
   ├─ ❏ ${prefix}setprefix
   ├─ ❏ ${prefix}block
   ├─ ❏ ${prefix}bc
   ├─ ❏ ${prefix}bcgc
-  ├─ ❏ ${prefix}clone
   └─ ❏ ${prefix}clearall
 
 ◪ *OTROS*
   │
   ├─ ❏ ${prefix}wame
+  ├─ Link de Whatsapp
   ├─ ❏ ${prefix}virtex (Hernqn)
   ├─ ❏ ${prefix}exe
   └─ ❏ ${prefix}qrcode
+  
+◪ *GRUPOS*
+  │
+  ├─ ❏ ${prefix}closecg
+  ├─ Cerrar el grupo solo admins
+  ├─ ❏ ${prefix}opencg
+  ├─ Abrir grupo solo admins
+  ├─ ❏ ${prefix}kick
+  ├─ Eliminar a un miembro 
+  ├─ ❏ ${prefix}promote
+  ├─ Dar admin a un miembro
+  ├─ ❏ ${prefix}demote
+  ├─ Quitar el admin
+  ├─ ❏ ${prefix}linkgc
+  ├─ Link del grupo
+  ├─ ❏ ${prefix}tagall
+  └─ Nombra a todos los del grupo
+
+Para usar estas funciones el bot necesita admin
   
 ◪ *NSWF*
   │
   └─ ❏ ${prefix}nsfwmenu
 
-     ❏ Para activar los NSFW coloque el siguiente comando ${prefix}nsfw1 y para desactivar los NSFW coloque ${prefix}nsfw0
+Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
 
-No te olvides de seguirme en instagram flaco
+No te olvides de seguirme en instagram flaco :)
+
+by Shanduy
+
+Ultima version mas actual del bot 
+https://github.com/shanduy/ShanBot
 `
 }
 
