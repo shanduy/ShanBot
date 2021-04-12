efc5391bc3b606f2585c28c78a9466e9d9909d8d
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-04-12 14:43:18 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -92,6 +92,7 @@ Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desact
 No te olvides de seguirme en instagram flaco :)
 
 by Shanduy
+
 Para saber la ultima version mas actual del bot entra en 
 https://github.com/shanduy/ShanBot
 `
