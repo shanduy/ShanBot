7234e66fac031fd15ed2b6f9f7dd82f8b081dc4a
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-05-20 15:43:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -363,10 +363,10 @@ async function starts() {
 										case 'nsfwmenu':
 											client.sendMessage(from, nsfwmenu(prefix, sender), text, {quoted: mek})
 													break
-               case 'virtex':
+               /*case 'virtex':
 	       case 'troleo':
                client.sendMessage(from, virtex(prefix, sender), text, {quoted: mek})
-               break
+               break*/
                case 'kodenegara':
                client.sendMessage(from, negara(prefix, sender), text, {quoted: mek})
                break
