fda01b77b89064b7afce60bc4a4103236a6b6191
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-05-26 12:59:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -111,27 +111,7 @@ const getLevelingXp = (userId) => {
                 return _level[position].level
             }
         }
-
-const ytmp3 = async (url) => new Promise((resolve, reject) => {
-    axios.get(`${link}/api/yta?url=${url}`)
-    .then((res) => {
-		resolve(res.data)
-    })
-    .catch((err) =>{
-        reject(err)
-    })
-})
-
-const ytmp4 = async (url) => new Promise((resolve, reject) => {
-    axios.get(`${link}/api/ytv?url=${url}`)
-    .then((res) => {
-		resolve(res.data)
-    })
-    .catch((err) =>{
-        reject(err)
-    })
-})
-
+	
 const getLevelingId = (userId) => {
             let position = false
             Object.keys(_level).forEach((i) => {
