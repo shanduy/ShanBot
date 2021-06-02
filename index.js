27defa8e2dfbba4b085ff70360a847bbdd69d860
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-01 19:14:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -250,10 +250,10 @@ async function starts() {
 					ownerB: '[❗] Este comando solo lo usa ShanBot! ❌',
 					admin: '[❗] Este comando solo puede ser utilizado por administradores del grupo! ❌',
 					Badmin: '[❗] Este comando solo se puede usar cuando el bot se convierte en administrador! ❌',
-                                        pegatina: 'Recuerda los stickers gifs son de 6 segundos\nby shanduy',
-					imgs: '❗Recuerda solo sirve para stickers❗\n\nConvirtiendo de sticker a imagen🔄\n\nby shanduy',
-					mpcancion: 'Calmao pa estoy procesando😎\n\nConvirtiendo de MP4 a MP3🔄\n\n\nby shanduy',
-					musica: 'Recuerda colocar bien el nombre de la cancion o el link del video de youtube\nby shanduy',
+                                        pegatina: 'Calma crack estoy haciendo tu sticker👏\n\n*❗Recuerda los stickersgif son de 6 segundos❗*\nby shanduy',
+					imgs: '❗Recuerda solo sirve para stickers❗\n\n*Convirtiendo de sticker a imagen🔄*\n\nby shanduy',
+					mpcancion: 'Calmaoooo estoy procesando😎\n\n*Convirtiendo de MP4 a MP3🔄*\n\n\nby shanduy',
+					musica: 'Calmao pa estoy bucando tu canción😎\n\n*❗Recuerda colocar bien el nombre de la cancion o el link del video de youtube❗*\n\nby shanduy',
 					daftarB: `「NEFASTOOOOO」\n\nPERO PAAAAAAAAAA!\n\nNo estas registrado en mi base de datos 😳 \n\nComando : ${prefix}daftar Nombre\nEjemplo : ${prefix}daftar shanduy`,
 				}
 			}
