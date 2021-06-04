05d8a02bd0dfa79a0efff03ececb2d3da5b396c0
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-04 15:03:21 -0500

    Create versión.js

---
+++ src/versión.js
@@ -0,0 +1,18 @@
+const versión = (prefix, pushname) => {
+    return `
+*ShanBot*
+
+*Actualizado:* 4 de junio de 2021
+*Versión actual:* 2.1
+*Ofrecida por:* shanduy
+
+Para conocer la ultima version de ShanBot entra al siguiente blog
+
+https://github.com/shanduy/ShanBot
+
+by shanduy
+`
+
+}
+
+exports.versión = versión
