c1c5a7da194fa2b4db71235b3751c5265b6b8d99
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-05 14:10:11 -0500

    Create welmenu.js

---
+++ src/welmenu.js
@@ -0,0 +1,20 @@
+const welmenu = (prefix, pushname) => {
+    return `
+*Comandos De Bienvenida 👋*
+
+*PARA ACTIVAR LA BIENVENIDA*
+
+- ${prefix}welcome 1
+
+*PARA DESACTIVAR LA BIENVENIDA*
+
+- ${prefix}welcome 0
+
+_Para activar este comando el bot necesita tener admin_
+
+by shanduy
+`
+
+}
+
+exports.welmenu = welmenu
