7f702d574283da82d2afc07353ce1e7348cbdc88
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-05 21:50:07 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -1,15 +1,15 @@
 const desmenu = (prefix, pushname) => {
-    return `◪ *Comandos De Descargas ✅*
-    │
-    ├─ ❏ ${prefix}play
-    │  Coloca el nombre de la cancion. 
-    │  O el link del video YT
-    ├─ ❏ ${prefix}ytmp3
-    │  Coloca el link del video YT
-    ├─❏ ${prefix}ytmp4
-    └─ Coloca el link del video YT
+    return `
+*Comandos De Descargas ✅*
+ 
+- ${prefix}play
+  Coloca el nombre de la cancion. O el link del video YT
+- ${prefix}ytmp3
+  Coloca el link del video YT
+- ${prefix}ytmp4
+  Coloca el link del video YT
     
-    _*play tiene un limite de 2000 canciones si llega a su limite se restablecera dentro de 24 horas_
+_*play tiene un limite de 2000 canciones si llega a su limite se restablecera dentro de 24 horas_
      
 by shanduy`
 
