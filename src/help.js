295dbdae7a1fd7140d2815f55c611895228321b1
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-05 22:14:17 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,84 +1,88 @@
 const help = (prefix) => {
 	return `
-「 *ShanBot 🤖 by shanduy* 」
+ ⌜ *ShanBot 🤖 by shanduy* ⌟  
 
-◪ *INFORMACION*
-  ❏ Comando: 「  ${prefix}  」
-  ❏ Creador: Shanduy
-  ❏ Como instalar el bot: https://www.youtube.com/watch?v=2LQSzEbpJ-M
-  ❏ Instagram: https://www.instagram.com/thepavos
+◉ *INFORMACION*
+  ○ Comando: ⌜ ${prefix} ⌟
+  ○ Creador: shanduy™ 
+  ○ Como instalar el bot: https://www.youtube.com/watch?v=2LQSzEbpJ-M
+  ○ Instagram: https://www.instagram.com/thepavos
 
-◪ *ShanBot Con Insultos O Quitarlos🤷‍♂️?*
+◉ *ShanBot Con Insultos O Quitarlos🤷‍♂️?*
 Responde esto en la siguiente encuesta
 https://forms.gle/4zzqRpV21CMGwsF57
 
-◪ *NUEVOS MENUS*
+◉ *NUEVOS MENUS*
 - ${prefix}desmenu
+Descargar musica y videos de YT
 - ${prefix}version
 Conoce la versión de tu bot
 - ${prefix}welmenu
 Comandos de bienvedia a grupos
 
-◪ *PARA USAR EL BOT*
-  ❏ Registrate con el comando ${prefix}daftar y tu nombre
+◉ *PARA USAR EL BOT*
+Registrate con el comando ${prefix}daftar y tu nombre
 
-◪ *RESUELVE TUS DUDAS*
-  │
-  ├─ ❏ ${prefix}creador
-  └─ Dudas o problemas aqui. NO SOY UN BOT
+◉ *RESUELVE TUS DUDAS*
+  ║
+  ╠ ○ ${prefix}creador
+  ╚ Dudas o problemas aqui. NO SOY UN BOT
 
-◪ *CREAR STICKERS*
-  │
-  ├─ ❏ ${prefix}sticker
-  ├─ ❏ ${prefix}stickergif
-  └─ 6 segundos de video
+◉ *CREAR STICKERS*
+  ║
+  ╠ ○ ${prefix}sticker
+  ╠ ○ ${prefix}stickergif
+  ╚ 6 segundos de video
 
-◪ *CONVERTIDORES*
-  ├─ ❏ ${prefix}toimg
-  ├─ De sticker a JPG
-  ├─ ❏ ${prefix}tomp3
-  └─ De video a MP3
+◉ *CONVERTIDORES*
+  ║
+  ╠ ○ ${prefix}toimg
+  ╠ De sticker a JPG
+  ╠ ○ ${prefix}tomp3
+  ╚ De video a MP3
 
-◪ *AUDIO*
-  │
-  ├─ ❏ ${prefix}bahasa
-  └─ ❏ ${prefix}tts es
+◉ *AUDIO*
+  ║
+  ╠ ○ ${prefix}bahasa
+  ╚ ○ ${prefix}tts es
 
-◪ *OTROS*
-  │
-  ├─ ❏ ${prefix}wame
-  ├─ Link de Whatsapp
-  ├─❏ ${prefix}qrcode
-  └─ Coloca un texto
+◉ *OTROS*
+  ║
+  ╠ ○ ${prefix}wame
+  ╠ Link de Whatsapp
+  ╠ ○ ${prefix}qrcode
+  ╚ Coloca un texto
 
-◪ *GRUPOS*
-  │
-  ├─ ❏ ${prefix}closegc
-  ├─ Cerrar el grupo solo admins
-  ├─ ❏ ${prefix}opengc
-  ├─ Abrir grupo solo admins
-  ├─ ❏ ${prefix}kick o pafuera
-  ├─ Eliminar a un miembro 
-  ├─ ❏ ${prefix}promote
-  ├─ Dar admin a un miembro
-  ├─ ❏ ${prefix}demote
-  ├─ Quitar el admin
-  ├─ ❏ ${prefix}linkgc
-  ├─ Link del grupo
-  ├─ ❏ ${prefix}tagall
-  └─ Nombra a todos los del grupo
+◉ *GRUPOS*
+  ║
+  ╠ ○ ${prefix}closegc
+  ╠ Cerrar el grupo solo admins
+  ╠ ○ ${prefix}opengc
+  ╠ Abrir grupo solo admins
+  ╠ ○ ${prefix}kick o pafuera
+  ╠ Eliminar a un miembro 
+  ╠ ○ ${prefix}promote
+  ╠ Dar admin a un miembro
+  ╠ ○ ${prefix}demote
+  ╠ Quitar el admin
+  ╠ ○ ${prefix}linkgc
+  ╠ Link del grupo
+  ╠ ○ ${prefix}tagall
+  ╚ Nombra a todos los del grupo
 
 Para usar estas funciones el bot necesita admin
   
-◪ *NSWF* 
-  │
-  └─ ❏ ${prefix}nsfwmenu
+◉ *NSWF* 
+  ║
+  ╚ ○ ${prefix}nsfwmenu
 
 Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
 
-No te olvides de seguirme en instagram flaco :)
+No te olvides de seguirme en instagram flaco ;)
 
 by shanduy
+
+⌜ *ごきげんよう :)* ⌟ 
 `
 }
 
