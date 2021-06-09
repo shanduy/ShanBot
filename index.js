e451e951b595ce76ee45b3f8a29b308801e7a076
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-08 21:48:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ async function starts() {
 			const from = mek.key.remoteJid
 			const type = Object.keys(mek.message)[0]
 			const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
-			const time = moment.tz('America/Guayaquil').format('DD/MM HH:mm:ss')
+			const time = moment.tz('America/Guayaquil').format('HH:mm:ss')
 			const date = moment.tz('America/Guayaquil').format('DD/MM/YY')
 			body = (type === 'conversation' && mek.message.conversation.startsWith(prefix)) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption.startsWith(prefix) ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption.startsWith(prefix) ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text.startsWith(prefix) ? mek.message.extendedTextMessage.text : ''
 			budy = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : ''
