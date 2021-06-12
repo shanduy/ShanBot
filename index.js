545e88dcb39f8bc668b976114d9bf444c82721fa
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-12 14:35:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const { virtex } = require('./src/virtex')
 const { wait, pegatinas, musica, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
 const { fetchJson } = require('./lib/fetcher')
 const { webp2mp4File } = require('./lib/webp2mp4')
+const { yta, ytv, igdl, upload } = require('./lib/ytdl')
 const { recognize } = require('./lib/ocr')
 /******FIN DE ENTRADA DE ARCHIVO******/
 
