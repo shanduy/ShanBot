d158cc290b9f18d8baca6078f3fdb15f962ef232
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-14 23:09:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -829,9 +829,7 @@ async function starts() {
 				case 'sticker':
 				case 'stickergif':
 				case 'stikergif':
-			        if (isLimit(sender)) return reply(ind.limitend(pusname))
-				await limitAdd(sender)
-					if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+			        if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
 						const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
 						const media = await client.downloadAndSaveMediaMessage(encmedia)
 						ran = getRandom('.webp')
