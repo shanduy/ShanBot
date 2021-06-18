0686f3d5e1f7ddc9552879165c198ba46c07c57a
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-18 14:54:57 -0500

    Create otak.js

---
+++ src/otak.js
@@ -0,0 +1,14 @@
+const otak = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes mis queridos otakus*
+
+- quien es tu sempai botsito
+- me gimes 7u7
+- te amo botsito uwu
+- onichan
+
+_Ojito escribe tal como esta_
+
+by shanduy
+}
+
+exports.otak = otak
