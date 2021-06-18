b5389b639e11f2bca48ced322bca326fd343b977
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-17 23:11:31 -0500

    Update shantera.js

--- src/shantera.js
+++ src/shantera.js
@@ -1,6 +1,7 @@
 const shantera = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes*
 
+
 - todo bien
 - buenos dias
 - bot gay
@@ -22,6 +23,17 @@ const shantera = (prefix, pushname) => {
 - una mierda de bot
 - ultimo momento
 - nefasto
+- paraguayo
+- bot de mierda
+- venezolano
+- gaspi corte
+- ya me voy a dormir
+- calefon
+- apurate bot
+- un chino
+- no funciona
+- boliviano
+
 
 _Ojito escribe tal y como esta en el mensaje_
 
