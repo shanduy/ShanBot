20ad4ae4ac0555b8207d42e0bd0b40377176afa8
Lalelilolu-2404 <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-06-23 03:14:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1261,16 +1261,17 @@ async function starts() {
         const none = fs.readFileSync('./anishan/Petardo.mp3');
 		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
                   }					
-        if (budy.startsWith(`A`)) and if (budy.endsWith(`A`)) {
+        if (budy.startsWith(`A`)) {
         	const none = fs.readFileSync('./anishan/A.mp3');
 		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
                   }
 	
-        if (budy.startsWith(`a`)) and if (budy.endsWith(`a`)) {
+        if (budy.startsWith(`a`)) {
+		if (budy.endsWith(`a`)){
         	const none = fs.readFileSync('./anishan/A.mp3');
 		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
                   }	
-		
+		}
         if (budy.startsWith(`Pasa pack`)) {
         const none = fs.readFileSync('./anishan/Pasa pack.mp3');
 		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
