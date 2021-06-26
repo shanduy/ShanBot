b237021de84e65bcc06caa3ad527ea6d613f5412
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-26 13:02:53 -0500

    Create juegos.js

---
+++ src/juegos.js
@@ -0,0 +1,16 @@
+const juegos = (prefix, pushname) => {
+    return `
+*Comandos De Juegos 👾*
+ 
+${prefix}gay
+Conoce que tan gay eres 😎
+${prefix}cuties
+Prueba tu suerte en el medidor de fan de cuties 😱
+
+_*En futuras versiones colocare mas juegos :)*_
+     
+by shanduy`
+
+}
+
+exports.juegos = juegos
