1ce1d42e52a8cc5884c9ece3d3cc3063d344f661
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-26 13:06:05 -0500

    Update shantera.js

--- src/shantera.js
+++ src/shantera.js
@@ -2,41 +2,42 @@ const shantera = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes*
 
 
-- todo bien
-- buenos dias
-- bot gay
-- gracias
-- hola
-- fua
-- corte
-- gaspi buenos dias 
-- gaspi me saludas
-- gaspi y las minitas
-- gaspi todo bien
-- me quiero suicidar
-- gaspi ya no aguanto
-- contate algo bot
-- sexo
-- momento epico
-- el bot del orto no funciona
-- epicardo
-- insta de la minita
-- una mierda de bot
-- ultimo momento
-- nefasto
-- paraguayo
-- bot de mierda
-- venezolano
-- gaspi corte
-- ya me voy a dormir
-- calefon
-- apurate bot
-- un chino
-- no funciona
-- boliviano
+todo bien
+buenos dias
+bot gay
+gracias
+hola
+fua
+corte
+gaspi buenos dias 
+gaspi me saludas
+gaspi y las minitas
+gaspi todo bien
+me quiero suicidar
+gaspi ya no aguanto
+contate algo bot
+sexo
+momento epico
+el bot del orto no funciona
+epicardo
+insta de la minita
+una mierda de bot
+ultimo momento
+nefasto
+paraguayo
+bot de mierda
+venezolano
+gaspi corte
+ya me voy a dormir
+calefon
+apurate bot
+un chino
+no funciona
+boliviano
+enano
 
 
-_Ojito escribe tal y como esta en el mensaje_
+_*Ojito escribe tal y como esta en el mensaje*_
 
 by shanduy`
 
