fa7a4da6cc3dcfa1f8725ffedb42a4d069524fb8
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-28 21:02:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -796,7 +796,7 @@ break
 						reply('❬ ✅ ❭ La funcion de antilink esta habilitada en este grupo')
 						client.sendMessage(from,`Atención a todos los miembros activos de este grupo 📣\n\nEl antilink esta activo, y si envían un enlace de otro grupo serán expulsados de este grupo de inmediato`, text)
 					} else if (Number(args[0]) === 0) {
-						if (!isantilink) return reply('El antilink ya esta deshabilitado')
+						if (isAntiLink) return reply('El antilink ya esta deshabilitado')
 						var ini = anti.clientOf(from)
 						antilink.splice(ini, 1)
 						fs.writeFileSync('./src/antilink.json', JSON.stringify(antilink))
