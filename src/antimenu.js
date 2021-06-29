4bf816ca36cc2158a7b207d80b903b8bf1d4ce3d
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-06-28 20:53:21 -0500

    Create antimenu.js

---
+++ src/antimenu.js
@@ -0,0 +1,18 @@
+const antimenu = (prefix, pushname) => {
+    return `
+*Comandos Para Activar El Antilink ⚠*
+
+*PARA ACTIVAR EL ANTILINK*
+${prefix}antilink 1
+
+*PARA DESACTIVAR EL ANTILINK*
+${prefix}antilink 0
+
+_Para activar este comando el bot necesita tener admin y esta funcion solo la activan los admins del grupo_
+
+by shanduy
+`
+
+}
+
+exports.antimenu = antimenu
