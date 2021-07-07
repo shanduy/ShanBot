19f712bbaf50582a81854724fa5e00177df090ae
Axel-bytu <86326762+Axel-bytu@users.noreply.github.com>
2021-07-06 22:26:50 -0500

    Update antimenu.js

--- src/antimenu.js
+++ src/antimenu.js
@@ -8,7 +8,13 @@ ${prefix}antilink 1
 *PARA DESACTIVAR EL ANTILINK*
 ${prefix}antilink 0
 
-_Para activar este comando el bot necesita tener admin y esta funcion solo la activan los admins del grupo_
+*PARA ACTIVAR EL ANTISPAM*
+${prefix}antispam 1
+
+*PARA DESACTIVAR EL ANTISPAM*
+${prefix}antispam 0
+
+_Para activar estos comando el bot necesita tener admin y esta funcion solo la activan los admins del grupo_
 
 by shanduy
 `
