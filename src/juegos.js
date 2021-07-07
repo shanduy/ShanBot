c4526cda56c5663b35e7003a3945146544b7ce55
Axel-bytu <86326762+Axel-bytu@users.noreply.github.com>
2021-07-06 22:51:47 -0500

    Update juegos.js

--- src/juegos.js
+++ src/juegos.js
@@ -6,6 +6,8 @@ ${prefix}gay
 Conoce que tan gay eres 😎
 ${prefix}cuties
 Prueba tu suerte en el medidor de fan de cuties 😱
+${prefix}cornu
+conoce que tan infiel te son las chicas 😵
 
 _*En futuras versiones colocare mas juegos :)*_
      
