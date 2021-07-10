3bef95b8624875249d2d5ab86f447287fc8597f9
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-07-10 15:02:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -457,8 +457,7 @@ async function starts() {
 			switch(command) {
 		case 'help':
 		case 'menu':
-                wew = fs.readFileSync(`./banner/shanbaner.jpg`)
-                    databaseuhy = `
+                databaseuhy = `
 
 ⌜ *ShanBot 🤖 by shanduy* ⌟  
 
@@ -552,7 +551,7 @@ by shanduy
 
 ⌜ *ごきげんよう :)* ⌟ 
 ` 
-                client.sendMessage(from, wew, image, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc ", "mimetype": "image/jpeg", "caption": "[ѕнαηвσт ву ѕнαη∂υу]", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./banner/shanlogo.jpg')} } }, caption: databaseuhy})
+                client.sendMessage(from, image, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc ", "mimetype": "image/jpeg", "caption": "[ѕнαηвσт ву ѕнαη∂υу]", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./banner/shanlogo.jpg')} } }, caption: databaseuhy})
 		break
                 case 'otak':
 		client.sendMessage(from, otak(prefix, sender), text, {quoted: mek})
