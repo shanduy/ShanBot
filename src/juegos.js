644af9576b44cfaa01171576618b635790027f66
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-07-11 19:41:57 -0500

    Update juegos.js

--- src/juegos.js
+++ src/juegos.js
@@ -4,8 +4,10 @@ const juegos = (prefix, pushname) => {
  
 ${prefix}rankgay
 Conoce a el top de los mas gays del grupo
+
 ${prefix}gay
 Conoce que tan gay eres 😎
+
 ${prefix}cuties
 Prueba tu suerte en el medidor de fan de cuties 😱
 
