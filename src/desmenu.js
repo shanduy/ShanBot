a0e536afae2f772b2d38c15e64754c077a96d0a4
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-07-11 19:41:33 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -2,14 +2,16 @@ const desmenu = (prefix, pushname) => {
     return `
 *Comandos De Descargas ✅*
  
-═════════════════════════════
+══════════════
 
 *AUDIO*
 
 ${prefix}play2
 Una alternativa por si *play llego a su limite de canciones
+
 ${prefix}play
 Coloca el nombre de la cancion, o el link del video YT
+
 ${prefix}ytmp3
 Coloca el link del video YT
 
@@ -17,7 +19,7 @@ Coloca el link del video YT
 
 ${prefix}ytmp3 https://www.youtube.com/xxxxxxxxxxxxx
 
-═════════════════════════════
+══════════════
 
 *VIDEO*
 ${prefix}ytmp4
@@ -27,7 +29,7 @@ Coloca el link del video YT
 
 ${prefix}ytmp4 https://www.youtube.com/xxxxxxxxxxxxx
 
-═════════════════════════════
+══════════════
     
 _*play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
      
