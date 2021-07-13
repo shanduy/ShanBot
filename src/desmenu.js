08f6d6887b45ec851155467574a4b92fbd2dedb3
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-07-13 16:53:28 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -33,7 +33,7 @@ ${prefix}ytmp4 https://www.youtube.com/xxxxxxxxxxxxx
     
 _*play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
      
-by shanduy`
+ву ѕнαη∂υу`
 
 }
 
