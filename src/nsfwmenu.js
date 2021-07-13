96b5615624d116677eb7d9ecb1fe7555c312cebd
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-07-13 16:53:09 -0500

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -11,7 +11,7 @@ _El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
 
 Algunas funciones fueron eliminas por errores en el servidor de la India
 
-by shanduy`
+ву ѕнαη∂υу`
 
 }
 
