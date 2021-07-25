b7d42abd2e61b3117d9c80f13abc072f6797bd3c
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-07-25 13:11:00 -0500

    Update otak.js

--- src/otak.js
+++ src/otak.js
@@ -2,14 +2,14 @@ const otak = (prefix, pushname) => {
     return `*Palabras especificas para que el bot interactue con ustedes mis queridos otakus*
 
 
-quien es tu sempai botsito
-me gimes 7u7
-te amo botsito uwu
-onichan
-la toca 7w7
+Quien es tu sempai botsito
+Me gimes 7u7
+Te amo botsito uwu
+Onichan
+La toca 7w7
 
 
-_Ojito escribe tal como esta_
+_*Ojito escribe tal como esta*_
 
 ву ѕнαη∂υу`
 
