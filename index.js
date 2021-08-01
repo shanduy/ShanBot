d221e0a2357cb037fa66012f8b422af8b26b9131
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-08-01 13:43:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -631,27 +631,7 @@ break
                 }
               await client.sendMessage(from, options, text)
                break
-                    case 'ytmp4':
-			if (args.length === 0) return reply(`Donde esta el link de youtube?\n\nEjemplo *${prefix}ytmp4 www.youtube.com/xxxxxxx*`)
-			let isLinks2 = args[0].match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/)
-			if (!isLinks2) return reply(mess.error.Iv)
-				try {
-				reply(mess.only.mpv)
-				ytv(args[0])
-				.then((res) => {
-				const { dl_link, thumb, title, filesizeF, filesize } = res
-				axios.get(`https://tinyurl.com/api-create.php?url=${dl_link}`)
-				.then((a) => {
-				if (Number(filesize) >= 40000) return sendMediaURL(from, thumb, `*DESCARGA EXITOSA ✅*\n\n*Tiutlo* : ${title}\n*Tamaño* : ${filesizeF}\n*Link* : ${a.data}\n\n_ESPERE ENVIANDO SU ARCHIVO MP4 ⚠_`)
-				const captionsYtmp4 = `*DESCARGA EXITOSA ✅*\n\n*Titulo* : ${title}\n*Formato* : MP4\n*Tamaño* : ${filesizeF}\n\n_ESPERE ENVIANDO SU ARCHIVO MP4 ⚠_`
-				sendMediaURL(from, thumb, captionsYtmp4)
-				sendMediaURL(from, dl_link).catch(() => reply(mess.error.api))
-				})		
-				})
-				} catch (err) {
-			    reply(mess.error.api)
-				}
-				break
+                    
 				
 			        case 'tts':
 				   client.updatePresence(from, Presence.recording) 
