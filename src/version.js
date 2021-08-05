bafd62cbcc26a8564ad157db2f21953ad52f516c
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-08-05 18:57:11 -0500

    Update version.js

--- src/version.js
+++ src/version.js
@@ -2,8 +2,8 @@ const version = (prefix, pushname) => {
     return `
 *ѕнαηвσт ву ѕнαη∂υу*
 
-*Actualizado:* 25 de julio de 2021
-*Versión actual:* 3.2
+*Actualizado:* 05 de Agosto del 2021
+*Versión actual:* 3.3
 *Ofrecida por:* ѕнαη∂υу™
 
 
