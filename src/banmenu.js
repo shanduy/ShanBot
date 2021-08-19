4c5c6a3bfdffa398b0894e80dcda005e0f3f65d1
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-08-18 21:31:35 -0500

    Create banmenu.js

---
+++ src/banmenu.js
@@ -0,0 +1,30 @@
+const banmenu = (prefix, pushname) => {
+    return `*Banear El Uso Del Bot*
+    
+ _El bot necesita de admin_
+
+Este comando impide que el baneado use el bot
+
+══════════════
+
+*PARA BANEAR*
+
+*ban + a la persona que deseas banear
+
+Ejemplo: *ban @xxxxxxx
+
+══════════════
+
+*PARA DESBANEAR*
+
+*desban + a la persona que deseas banear
+
+Ejemplo: *desban @xxxxxxx
+
+══════════════
+
+ву ѕнαη∂υу`
+
+}
+
+exports.banmenu = banmenu
