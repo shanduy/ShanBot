f300e9a39957f0ed7aa52dc12ef60cc676053834
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-08-26 15:40:47 -0500

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,8 @@
 const config = {
         botName: 'ShanBot',
         ownerName: 'Shan',
+        "zeksKey": "shanduy46",
+        "apiUrl": "https://api.zeks.me",
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
