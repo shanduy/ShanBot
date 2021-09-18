acb8000dabfb424ceb982b92b8b350c330f31744
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-09-18 17:24:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -652,7 +652,7 @@ if (budy.includes("https://m.facebook.com/")){
 		case 'help':
 		case 'menu':
 		//
-		var menu = `
+		menu = `
 
 ⌜ *ѕнαηвσт ву ѕнαη∂υу* ⌟  
 ◉ *INFORMACION*
@@ -746,8 +746,8 @@ No te olvides de seguirme en instagram flaco ;)
 _*by shanduy*_
 ⌜ *ごきげんよう :)* ⌟
 `
-                faketokoforwaded(menu)
-           	break
+                client.sendMessage(from, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc ", "mimetype": "image/jpeg", "caption": "ShanBot", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('banner/shanlogo.jpg')} } }, caption: menu})
+		break
 		
                 case 'otak':
 		client.sendMessage(from, otak(prefix, sender), text, {quoted: mek})
