e5bb48135f7dc1756d2a410da14aab7062c742b8
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-09-18 17:08:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,35 @@ const antidiscord = JSON.parse(fs.readFileSync('./src/antidiscord.json'))
 
 /******FIN DE ARCHIVOS ANTILINK POR SHANDUY******/
 
+//FAKE
 
+const faketokoforwaded = (teks) => {
+	anu = {
+	  key: {
+			fromMe: false,
+			participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
+		},
+		message: {
+			"productMessage": {
+				"product": {
+					"productImage":{
+						"mimetype": "image/jpeg",
+						"jpegThumbnail": fs.readFileSync(`./banner/shanlogo.jpg`)
+					},
+					"retailerId": "ShanBot",
+					"productImageCount": 1
+				},
+				"businessOwnerJid": `0@s.whatsapp.net`
+		}
+	}
+}
+	client.sendMessage(from, teks, text, {
+	  quoted: anu,
+	  contextInfo:{
+	    "forwardingScore": 999, "isForwarded": true
+	  }
+	})
+}
 
 
 //LEVEL INICIO
@@ -622,9 +650,105 @@ if (budy.includes("https://m.facebook.com/")){
 			}
 			switch(command) {
 		case 'help':
-		case 'menu':   
-                client.sendMessage(from, help(prefix, sender), text, {quoted: mek})
-		break
+		case 'menu':
+		//
+		var menu = `
+
+⌜ *ѕнαηвσт ву ѕнαη∂υу* ⌟  
+◉ *INFORMACION*
+   ○ Comando: ⌜ ${prefix} ⌟
+   ○ Creador: ѕнαη∂υу™ 
+   ○ Como instalar el bot: https://www.youtube.com/watch?v=2LQSzEbpJ-M
+   ○ Instagram: https://www.instagram.com/thepavos
+◉ *NUEVOS COMANDOS*
+○ ${prefix}banmenu
+Banear a personas el uso del bot
+○ ${prefix}levelmenu
+Level para tus grupos
+○ ${prefix}antimenu
+Su nombre lo dice todo (antilink)
+○ ${prefix}otak
+Monas chinas
+○ ${prefix}shantera
+Interactua con el bot
+◉ *NUEVOS MENUS*
+○ ${prefix}juegos
+Diviértete con tus amigos :)
+○ ${prefix}desmenu
+Descargar musica y videos
+○ ${prefix}version
+Conoce la versión de tu bot
+○ ${prefix}welmenu
+Comando de bienvenida solo grupos
+◉ *PARA USAR EL BOT*
+Registrate con el comando ${prefix}daftar y tu nombre
+◉ *RESUELVE TUS DUDAS*
+  ║
+  ╠ ○ ${prefix}creador
+  ╚ Dudas o problemas aqui
+◉ *CREAR STICKERS*
+  ║
+  ╠ ○ ${prefix}sticker
+  ╠ ○ ${prefix}attp
+  ╠ Mas un texto corto
+  ╠ ○ ${prefix}stickergif
+  ╚ 6 segundos de video
+◉ *CONVERTIDORES*
+  ║
+  ╠ ○ ${prefix}toimg
+  ╠ De sticker a JPG
+  ╠ ○ ${prefix}tomp3
+  ╚ De video a MP3
+◉ *AUDIO*
+  ║
+  ╠ ○ ${prefix}idioma
+  ╚ ○ ${prefix}tts es (mas texto)
+◉ *OTROS*
+  ║
+  ╠ ○ ${prefix}wame
+  ╠ Link de Whatsapp
+  ╠ ○ ${prefix}qrcode
+  ╚ Coloca un texto
+◉ *GRUPOS*
+  ║
+  ╠ ○ ${prefix}unir
+  ╠ Unir a una persona al grupo
+  ╠ ○ ${prefix}fgc
+  ╠ Cambiar foto del grupo
+  ╠ ○ ${prefix}ngc
+  ╠ Cambiar nombre del grupo
+  ╠ ○ ${prefix}dgc
+  ╠ Cambiar descripcion del grupo
+  ╠ ○ ${prefix}closegc
+  ╠ Cerrar el grupo solo admins
+  ╠ ○ ${prefix}opengc
+  ╠ Abrir grupo solo admins
+  ╠ ○ ${prefix}kick
+  ╠ Eliminar a un miembro 
+  ╠ ○ ${prefix}promote
+  ╠ Dar admin a un miembro
+  ╠ ○ ${prefix}demote
+  ╠ Quitar el admin
+  ╠ ○ ${prefix}linkgc
+  ╠ Link del grupo
+  ╠ ○ ${prefix}adminlist
+  ╠ Nombra a los admins del grupo
+  ╠ ○ ${prefix}todos
+  ╚ Nombra a todos los del grupo
+
+Para usar estas funciones el bot necesita admin
+  
+◉ *NSWF* 
+  ║
+  ╚ ○ ${prefix}nsfwmenu
+Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
+No te olvides de seguirme en instagram flaco ;)
+_*by shanduy*_
+⌜ *ごきげんよう :)* ⌟
+`
+                faketokoforwaded(menu)
+           	break
+		
                 case 'otak':
 		client.sendMessage(from, otak(prefix, sender), text, {quoted: mek})
 		break
