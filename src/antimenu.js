9e537d5ac42525d7bfca0a4240cdd8903ac81c36
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-09-18 12:55:10 -0500

    Update antimenu.js

--- src/antimenu.js
+++ src/antimenu.js
@@ -70,7 +70,7 @@ ${prefix}antidiscord 0
 _Para activar estos comandos el bot necesita tener admin en el grupo y esta funcion solo la activan los admins del grupo_
 
 
-ву ѕнαη∂υу
+_*by shanduy*_
 `
 
 }
