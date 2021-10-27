08697b3f2ff36fd74dce56dc5f8181017790a00b
Shanduy <79606488+shanduy@users.noreply.github.com>
2021-10-27 13:01:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1730,6 +1730,11 @@ break
                   reply(`el diegote pa`)
                   }
 	
+					
+	if (budy.startsWith(`VAMOOO`)) {
+        const none = fs.readFileSync('./mp3/vamo.mp3');
+		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
+                  }
 	if (budy.startsWith(`Hora del sexito`)) {
         const none = fs.readFileSync('./mp3/maau1.mp3');
 		client.sendMessage(from, none, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
