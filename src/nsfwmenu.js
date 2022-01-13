ea502bd2907dbdce84d8b16fe7bdfcfa7ad6c4bf
Shanduy <79606488+shanduy@users.noreply.github.com>
2022-01-13 13:18:19 -0500

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -2,17 +2,16 @@ const nsfwmenu = (prefix, pushname) => {
     return `*Comandos para ver pornito 🔞*
 *NSFW ✅*
 
-
 ${prefix}culos
 ${prefix}tetas
 ${prefix}ahegao
 ${prefix}pies
 ${prefix}rule34
 ${prefix}ecchi
-${prefix}yaoi "pa las chicas no solo los hombre se divierten"
+${prefix}neko
+${prefix}yaoi 
 
-
-_El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
+"El yaoi es pa las chicas no solo los hombre se divierten"
 
 by shanduy`
 
