8648f683133f85fa2123524b7762a896da401318
Shanduy <79606488+shanduy@users.noreply.github.com>
2022-01-13 12:48:03 -0500

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -2,16 +2,19 @@ const nsfwmenu = (prefix, pushname) => {
     return `*Comandos para ver pornito 🔞*
 *NSFW ✅*
 
-- ${prefix}nsfwbobs
-- ${prefix}nsfwsidebobs
-- ${prefix}nsfwahegao
-- ${prefix}nsfwfeets
+
+${prefix}culos
+${prefix}tetas
+${prefix}ahegao
+${prefix}pies
+${prefix}rule34
+${prefix}ecchi
+${prefix}yaoi "pa las chicas no solo los hombre se divierten"
+
 
 _El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
 
-Algunas funciones fueron eliminas por errores en el servidor de la India
-
-ву ѕнαη∂υу`
+by shanduy`
 
 }
 
