06460750e68ee42e2b2dc9ecf0932c384a12f311
Shanduy <79606488+shanduy@users.noreply.github.com>
2022-09-25 15:09:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const {
        Mimetype, 
        rugaapi, 
        GroupSettingChange 
-} = require ('@adiwajshing/baileys')
+} = require('@adiwajshing/baileys')
 
 /******COMIENZO DE LA ENTRADA DEL ARCHIVO******/
 const { color, bgcolor } = require('./lib/color');
